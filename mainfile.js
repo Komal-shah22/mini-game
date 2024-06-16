d917469a41da9f74afbae2a507181e921bc96cac
Komal-shah22 <160298356+Komal-shah22@users.noreply.github.com>
2024-06-16 10:50:44 +0100

    Add files via upload

---
+++ mainfile.js
@@ -0,0 +1,32 @@
+#!usr/bin/env node
+import inquirer from "inquirer";
+import { continueCode, data, headAndTail, numGuessGame, starMakingGame } from "./game.js";
+await data();
+const mainfunc = async () => {
+    const data = await inquirer.prompt([
+        {
+            name: 'seclect',
+            type: 'list',
+            message: 'Hears are some games for you',
+            choices: ['Star Making Game', 'Number Guessing', 'Head and Tail']
+        }
+    ]);
+    switch (data.seclect) {
+        case 'Star Making Game': {
+            await starMakingGame();
+            await continueCode(mainfunc);
+            break;
+        }
+        case 'Number Guessing': {
+            await numGuessGame();
+            await continueCode(mainfunc);
+            break;
+        }
+        case 'Head and Tail': {
+            await headAndTail();
+            await continueCode(mainfunc);
+            break;
+        }
+    }
+};
+mainfunc();
